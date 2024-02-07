adb50a4f8dbf5c17a05f4784efb1614794fc0c79
Tandon Jenkins <tjdjj12@gmail.com>
2024-02-07 02:00:31 +0000

    2.5 done, removed login from index

--- portfolio_app/portfolio_app/urls.py
+++ portfolio_app/portfolio_app/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
 #path function defines a url pattern
 #'' is empty to represent based path to app
-# views.index is the function defined in views.py
-# name='index' parameter is to dynamically create url
+# views.index is the function  defined in views.py
+# name='index parameter is to dynamically create url
 # example in html <a href="{% url 'index' %}">Home</a>.
 path('', views.index, name='index'),
 ]

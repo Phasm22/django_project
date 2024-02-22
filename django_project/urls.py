902ba8ed8f65a94de6061787231293dbdd5e1753
Tandon Jenkins <tjdjj12@gmail.com>
2024-02-22 21:20:11 +0000

    ge03 start

--- django_project/urls.py
+++ django_project/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 urlpatterns = [
 path('admin/', admin.site.urls),
-#connect path to portfolio_app urls
+#connect path to portfolio_app url s
 path('', include('portfolio_app.urls')),
 ]
